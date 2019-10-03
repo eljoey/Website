2731c039ffc80f713ca3a7af13d7134f55bc0469
Joe Hayden <eljoeyy@gmail.com>
2019-10-03 16:25:06 -0500

    uploaded to test mobile responsiveness changes

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
   speed: 700
 })
+
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.navbar ul')
+const links = document.querySelectorAll('.navbar li')
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open')
+})
